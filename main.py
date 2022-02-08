670005bb8ddc0658325e1cf7db3a5eff40f949f2
danmuck <danmuck426@gmail.com>
2022-02-08 12:33:26 -0500

    init commit --reissue #1

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import os, json, datetime
 
-import modules.coin_gecko
+def menu_(choices:list):
+    menu_opts = {}
+    i, j = 0, len(choices)
+    while i < j:
+        if i == 0:
+            menu_opts.update({i: "[QUIT]"})
+        else:
+            menu_opts.update({i: choices[i]})
+        i+=1
+    print(menu_opts)
 
 
+def main():
+    choices = [
+        "[Tasks]",
+        "[Personal]",
+        "[Tools]",
+        "[Projects]",
+        "[Finance]",
+        "[Users]",
+        "[Learning Tools]",
+        "[Games]",
+    ]
+    menu_(choices)
+
+main()
