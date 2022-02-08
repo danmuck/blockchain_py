d3e849cb043c4654779efd214f567eaa6898f7e3
danmuck <danmuck426@gmail.com>
2022-02-08 17:15:22 -0500

    func: main_menu_:main :: playing with match case for menu template

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from modules import finance_, Menu
+from modules import finance_
 import os, json, datetime
+
+
 class Menu_:
-    def __init__(self):
+    def __init__(self, main_menu, finance_menu):
         main_menu = (
             "[Tasks]",
             "[Personal]",
@@ -11,17 +13,13 @@ class Menu_:
             "[Users]",
             "[Learning Tools]",
             "[Games]",
-)
-        main_menu_opts = {}
-        i, j = 0, len(main_menu)
-        while i < j:
-            if i == 0:
-                main_menu_opts.update({i: "[QUIT]"})
-            else:
-                main_menu_opts.update({i: main_menu[i]})
-            i+=1
+            )
+        finance_menu = (
+            "[Calculate Monthly Interest Gains]"
+        )
 
-        self.main_menu_opts = main_menu_opts
+        self.main_menu_opts = self.format_menu_(main_menu)
+        self.finance_menu_opts = self.format_menu_(finance_menu)
 
     def format_menu_(self, menu_:list) -> dict:
         '''
@@ -33,44 +31,145 @@ class Menu_:
         menu_opts_ = {}
         i, j = 0, len(menu_)
         while i < j:
-            if i == 0:
-                menu_opts_.update({"q": "[QUIT]"})
-            else:
-                menu_opts_.update({i: menu_[i]})
+            menu_opts_.update({i: menu_[i]})
             i+=1
+        menu_opts_.update({"q": "[QUIT]"})
         return menu_opts_
 
-    def print_menu_(self):
-        for key, value in self.main_menu_opts.items():
-            print(f"{key}: {value} ")
 
     def main_menu_(self):
+        '''
+            Obnoxious menu copy/paste template to cover 10 cases with KeyError handling 
+                for ease in development.
+                    Remove excess cases for production.
+        '''
+        menu_dict = self.main_menu_opts
+        print_dict(menu_dict)
+        select = input("Where to? \n: ")
+
+        match select:
+            case "0":
+                try:
+                    print(menu_dict[0])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()
+            case "1":
+                try:
+                    print(menu_dict[1])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()
+            case "2":
+                try:
+                    print(menu_dict[2])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()
+            case "3":
+                try:
+                    print(menu_dict[3])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()                        
+            case "4":
+                try:
+                    print(menu_dict[4])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()                        
+            case "5":
+                try:
+                    print(menu_dict[5])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()                        
+            case "6":
+                try:
+                    print(menu_dict[6])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()                        
+            case "7":
+                try:
+                    print(menu_dict[7])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()                        
+            case "8":
+                try:
+                    print(menu_dict[8])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()                        
+            case "9":
+                try:
+                    print(menu_dict[9])
+                except KeyError:
+                    pass
+                finally:
+                    self.main_menu_()                        
+            case "q":
+                print(menu_dict["q"])
+                exit()
+            case _:
+                print("\n[oops...]\n")
+                self.main_menu_()
+
+    def finance_menu_(self):
+        menu_dict = self.finance_menu_opts
+        print_dict(menu_dict)
         select = int(input("Where to? \n: "))
         match select:
-            case 1:
-                print(self.main_menu_opts[1])
-            case 2:
-                print(self.main_menu_opts[2])
-            case 3:
-                print(self.main_menu_opts[3])
-            case 4:
-                print(self.main_menu_opts[4])
-            case 5:
-                print(self.main_menu_opts[5])
-            case 6:
-                print(self.main_menu_opts[6])
-            case 7:
-                print(self.main_menu_opts[7])
-            case 8:
-                print(self.main_menu_opts[8])        
-            case 9:
-                print(self.main_menu_opts[9])
-            case q:
-                print(self.main_menu_opts[0])
+            case "0":
+                try:
+                    print(menu_dict[0])
+                except KeyError:
+                    pass
+                finally:
+                    self.finance_menu_()
+            case "1":
+                try:
+                    print(menu_dict[1])
+                except KeyError:
+                    pass
+                finally:
+                    self.finance_menu_()
+            case "q":
+                print(menu_dict[0])
                 exit()
+        self.finance_menu_()
 
-menu = Menu_
+def print_dict(menu_opts:dict):
+    for key, value in menu_opts.items():
+        print(f"{key}: {value} ")
 
+menu = Menu_(
+        main_menu = (
+            "[Tasks]",
+            "[Personal]",
+            "[Tools]",
+            "[Projects]",
+            "[Finance]",
+            "[Users]",
+            "[Learning Tools]",
+            "[Games]",
+            ),
+        finance_menu = (
+            "[Calculate Monthly Interest Gains]"
+        )
+)
+
+menu.main_menu_()
 
 
 
