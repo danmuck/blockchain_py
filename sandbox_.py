b9774377902a502e1475122842f011ba97fc5937
danmuck <danmuck426@gmail.com>
2022-02-09 02:12:04 -0500

    min: sandbox_ :: moved into class

--- sandbox_.py
+++ sandbox_.py
@@ -39,22 +39,25 @@ UDBBLS_LIST = [
 
     ]
 class Minter:
-    def __init__(self, iters_:int) -> None:
+    def __init__(self, name_:str, iters_:int, sleep_time:float) -> None:
         self.unr_16_ = [] # rares
         self.udbbls_ = [] # uncommon
         self.others_ = [] # some fun ones
         self.zeroes_ = [] # literal zeroes
         self.ovr999_ = [] # over 999
         self.common_ = []
-        self.start_time = time.time()
+        self.name_ = name_
+        self.start_time = float
         self.iters_ = iters_
+        self.sleep_time = sleep_time
 
     def generator(self):
+        self.run_timer()
         nanos_ = (time.time_ns() + randint(1, 4096) * math.pi)
         i, j = 1, self.iters_
         while i <= j:
             i+=1
-            time.sleep(.0025)
+            time.sleep(self.sleep_time)
             ez_nums = int( ((nanos_ * randint(1, 4097)) * int(time.strftime('%S'))) % 1023 )
             if ez_nums <= 15 and ez_nums != 0:
                 print(f"!! --   {ez_nums}   \t -- !!")
@@ -73,7 +76,10 @@ class Minter:
                 self.ovr999_.append(ez_nums)
             else:
                 # print(ez_nums)
-                self.common_.append(ez_nums)        
+                self.common_.append(ez_nums)
+        self.end_timer()
+        self.summary_to_console()
+        self.print_log_txt()
 
     def get_percents_(self) -> list:
         unr_16_p = (len(self.unr_16_) / self.iters_) * 100
@@ -92,12 +98,18 @@ class Minter:
         ]
         return percents_
 
+    def run_timer(self):
+        self.start_time = time.time()
+        return self.start_time
+
+    def end_timer(self):
+        return round(time.time() - self.start_time, 2)
 
     def summary_to_console(self):
         print(f"""
     Data:
 
-    Ez_nums: {sorted(self.unr_16_)}
+    Unr_16_: {sorted(self.unr_16_)}
 
     Zeros__: {sorted(self.zeroes_)}
 
@@ -108,11 +120,11 @@ class Minter:
     Common_: [disabled for now]
         """)
         print(f"""
-        {j} Iterations took {round(time.time() - self.start_time, 2)}sec 
+        {self.iters_} Iterations took {self.end_timer()}sec 
             accuracy check -- {round(sum(self.get_percents_()), 2)}%
 
-            Ez_nums: {len(self.unr_16_)}   \t\t{round((self.get_percents_())[0], 2)}%   
-            udbbls_: {len(self.udbbls_)}   \t\t{round((self.get_percents_())[1], 2)}%   
+            Unr_16_: {len(self.unr_16_)}   \t\t{round((self.get_percents_())[0], 2)}%   
+            Udbbls_: {len(self.udbbls_)}   \t\t{round((self.get_percents_())[1], 2)}%   
             Others_: {len(self.others_)}   \t\t{round((self.get_percents_())[2], 2)}% 
             Zeros__: {len(self.zeroes_)}   \t\t{round((self.get_percents_())[3], 2)}% 
             Ovr999_: {len(self.ovr999_)}   \t\t{round((self.get_percents_())[4], 2)}% 
@@ -123,171 +135,53 @@ class Minter:
 
     # FILES ---
     def print_log_txt(self):
-        with open(f'{os.getcwd()}/sandbox_/iterations.txt', 'a') as file:
+        with open(f'{os.getcwd()}/sandbox_/{self.name_}.txt', 'a') as file:
             file.write(f"""
-        {j} Iterations took {round(time.time() - start_time, 2)}sec 
-            accuracy check -- {round(sum(self.get_percents_()), 2)}%
+    {self.iters_} Iterations took {self.end_timer()}sec 
+        accuracy check -- {round(sum(self.get_percents_()), 2)}%
 
-            Ez_nums: {len(self.unr_16_)}   \t\t{round((self.get_percents_())[0], 2)}%   
-            udbbls_: {len(self.udbbls_)}   \t\t{round((self.get_percents_())[1], 2)}%   
-            Others_: {len(self.others_)}   \t\t{round((self.get_percents_())[2], 2)}% 
-            Zeros__: {len(self.zeroes_)}   \t\t{round((self.get_percents_())[3], 2)}% 
-            Ovr999_: {len(self.ovr999_)}   \t\t{round((self.get_percents_())[4], 2)}% 
-            Common_: {len(self.common_)}     \t{round((self.get_percents_())[5], 2)}%
+        Unr_16_: {len(self.unr_16_)}   \t\t{round((self.get_percents_())[0], 2)}%   
+        Udbbls_: {len(self.udbbls_)}   \t\t{round((self.get_percents_())[1], 2)}%   
+        Others_: {len(self.others_)}   \t\t{round((self.get_percents_())[2], 2)}% 
+        Zeros__: {len(self.zeroes_)}   \t\t{round((self.get_percents_())[3], 2)}% 
+        Ovr999_: {len(self.ovr999_)}   \t\t{round((self.get_percents_())[4], 2)}% 
+        Common_: {len(self.common_)}     \t{round((self.get_percents_())[5], 2)}%
 
         """)
 
 
 
+    def json_record(self, file_name:str):
+        try:
+            with open(f'{os.getcwd()}/sandbox_/data.json', 'x') as file:
+                json_obj_ = self.jsonify_("init_run", self.unr_16_, self.udbbls_, self.others_, self.zeroes_, self.ovr999_)
+                new_file = json.dumps(json_obj_, indent=2)
+                file.write(new_file)
+        except FileExistsError:
+            print("Err!! Filename in use.")
+        except FileNotFoundError:
+            with open(f'{os.getcwd()}/sandbox_/data.json', 'x') as file:
+                json_fp = self.jsonify_("init_run", self.unr_16_, self.udbbls_, self.others_, self.zeroes_, self.ovr999_)
+                file.write(json_fp)
 
-
-
-
-def hmm_idea(iters_:int):
-
-    common_ = []
-    unr_16_ = [] # rares
-    ovr999_ = [] # over 999
-    zeroes_ = [] # literal zeroes
-    others_ = [] # some fun ones
-    udbbls_ = [] # uncommon
-
-    start_time = time.time()
-    nanos_ = (time.time_ns() + randint(1, 4096) * math.pi)
-    i, j = 1, iters_
-    while i <= j:
-        i+=1
-        time.sleep(.0025)
-        ez_nums = int( ((nanos_ * randint(1, 4097)) * int(time.strftime('%S'))) % 1023 )
-
-        if ez_nums <= 15 and ez_nums != 0:
-            print(f"!! --   {ez_nums}   \t -- !!")
-            unr_16_.append(ez_nums)
-        elif ez_nums == 0:
-            print(f"!! --   {ez_nums}   \t -- !!")
-            zeroes_.append(ez_nums)
-        elif ez_nums > 999:
-            print(f"!! --   {ez_nums}   \t -- !!")
-            ovr999_.append(ez_nums)
-        elif ez_nums in OTHERS_LIST:
-            print(f"!! --   {ez_nums}   \t -- !!")
-            others_.append(ez_nums)
-        elif ez_nums in UDBBLS_LIST:
-            print(f"!! --   {ez_nums}   \t -- !!")
-            udbbls_.append(ez_nums)            
-        else:
-            # print(ez_nums)
-            common_.append(ez_nums)
-
-    unr_16_p = (len(unr_16_) / j) * 100
-    zeroes_p = (len(zeroes_) / j) * 100
-    ovr999_p = (len(ovr999_) / j) * 100
-    others_p = (len(others_) / j) * 100
-    common_p = (len(common_) / j) * 100
-    udbbls_p = (len(udbbls_) / j) * 100
-
-
-    percents_ = [
-        unr_16_p,
-        zeroes_p,
-        ovr999_p,
-        others_p,
-        common_p,
-        udbbls_p,
-    ]
-
-
-
-
-
-    print(f"""
-Data:
-
-Ez_nums: {sorted(unr_16_)}
-
-Zeros__: {sorted(zeroes_)}
-
-Ovr999_: {sorted(ovr999_)}
-
-Others_: {sorted(others_)}
-
-Common_: [disabled for now]
-    """)
-
-    print(f"""
-    {j} Iterations took {round(time.time() - start_time, 2)}sec 
-        accuracy check -- {round(sum(percents_), 2)}%
-
-        Common_: {len(common_)}     \t{round(common_p, 2)}%
-        Ez_nums: {len(unr_16_)}   \t\t{round(unr_16_p, 2)}%   
-        Zeros__: {len(zeroes_)}   \t\t{round(zeroes_p, 2)}% 
-        Ovr999_: {len(ovr999_)}   \t\t{round(ovr999_p, 2)}% 
-        Others_: {len(others_)}   \t\t{round(others_p, 2)}% 
-        udbbls_: {len(udbbls_)}   \t\t{round(udbbls_p, 2)}%   
-
-    """)
-
-
-# FILES ---
-
-    with open(f'{os.getcwd()}/sandbox_/iterations.txt', 'a') as file:
-        file.write(f"""
-    {j} Iterations took {round(time.time() - start_time, 2)}sec 
-        accuracy check -- {round(sum(percents_), 2)}%
-
-        Common_: {len(common_)}     \t{round(common_p, 2)}%   
-        Ez_nums: {len(unr_16_)}   \t\t{round(unr_16_p, 2)}%   
-        Zeros__: {len(zeroes_)}   \t\t{round(zeroes_p, 2)}% 
-        Ovr999_: {len(ovr999_)}   \t\t{round(ovr999_p, 2)}% 
-        Others_: {len(others_)}   \t\t{round(others_p, 2)}% 
-        udbbls_: {len(udbbls_)}   \t\t{round(udbbls_p, 2)}%   
-
-    """)
-    with open(f'{os.getcwd()}/sandbox_/nc_data.txt', 'a') as file:
-        file.write(f"""
-Data:
-
-Ez_nums: {sorted(unr_16_)}
-
-Zeros__: {sorted(zeroes_)}
-
-Ovr999_: {sorted(ovr999_)}
-
-Others_: {sorted(others_)}
-
-Common_: disabled
-
-    """)
-
-def json_record(file_name:str):
-    try:
-        with open(f'{os.getcwd()}/sandbox_/data.json', 'x') as file:
-            json_obj_ = jsonify_("init_run", unr_16_, udbbls_, others_, zeroes_, ovr999_)
-            new_file = json.dumps(json_obj_, indent=2)
-            file.write(new_file)
-    except FileExistsError:
-        print("Err!! Filename in use.")
-    except FileNotFoundError:
-        with open(f'{os.getcwd()}/sandbox_/data.json', 'x') as file:
-            json_fp = jsonify_("init_run", unr_16_, udbbls_, others_, zeroes_, ovr999_)
-            file.write(json_fp)
-
-def jsonify_(title:str, unr_16:list, udbbls:list, others:list, zeroes:list, ovr999:list) -> dict:
-    new_dict = {
-        f"{title}": {
-            "UNR_16": unr_16,
-            "udbbls": udbbls,
-            "others": others,
-            "zeroes": zeroes,
-            "ovr999": ovr999,
+    def jsonify_(self, title:str, unr_16:list, udbbls:list, others:list, zeroes:list, ovr999:list) -> dict:
+        new_dict = {
+            f"{title}": {
+                "UNR_16": unr_16,
+                "udbbls": udbbls,
+                "others": others,
+                "zeroes": zeroes,
+                "ovr999": ovr999,
+            }
         }
-    }
-    json_list = json.dumps(new_dict, indent=4)
-    return new_dict
+        json_list = json.dumps(new_dict, indent=4)
+        return new_dict
 
 
 
 
 def main():
-    hmm_idea(250)
+    # hmm_idea(250)
+    trial = Minter("Test_Minter", 2500, .0025)
+    trial.generator()
 main()
