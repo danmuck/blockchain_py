928381447dbf3e73b4c48dc20d505b4888ebed6b
danmuck <danmuck426@gmail.com>
2022-02-10 13:32:15 -0500

    re: main:modules :: moved stuff around

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from modules.Menus_ import Menu_
-from chain_ctl.Minter import Minter_
-from chain_ctl.Blockchain import Blockchain_
+from modules.chain_ctl.Minter import Minter_
+from modules.chain_ctl.Blockchain import Blockchain_
+from modules.chain_ctl.Block import Block_
 # from chain_ctl.Block import Block_
 
 menu = Menu_(
@@ -19,12 +20,22 @@ menu = Menu_(
         )
 )
 def main():
-    trial = Minter_("Minter_Sleep", 100000, .000002)
+    trial = Minter_("Minter", 100000, .000002)
     trial.generator()
     # trial.check_for_uniques()
     trial.update_history_json()
 
     bc = Blockchain_(0)
+    block_1 = Block_(
+                index=1,
+                previous_hash = bc.genesis_block.block_hash,
+                nonce = 0,
+                txns = [],
+                signature = 'im the second unverified block :)',
+                chain_data = {},
+            )
+
+    bc.append_block_(block_1)
     print(bc.chain)
     # menu.main_menu_()
 
