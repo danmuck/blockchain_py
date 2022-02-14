70c126c164e8c9cf176c9a14e4c9f0325afb454f
danmuck <danmuck426@gmail.com>
2022-02-13 21:58:10 -0500

    re: main :: formatting

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 
 
-import json, time
-from random import randint
+
+
 from modules.Menus_ import Menu_
-from modules.chain_ctl.Minter import Minter_
-from modules.chain_ctl.Blockchain import Blockchain_
-from modules.chain_ctl.Block import Block_
-from modules.chain_ctl.Proof_of_Work import Proof_of_Work
+
 
 menu = Menu_(
         main_menu = (
@@ -46,17 +43,6 @@ menu = Menu_(
 
 
 '''
-class Timer:
-    def __init__(self) -> None:
-        self.start_time = float
-    def start_timer(self):
-        self.start_time = time.time()
-        return self.start_time
-    def end_timer(self):
-        return round(time.time() - self.start_time, 2)
-timer = Timer()
-
-
 
 def main():
 
