36e0267f3104d47d9673f15b59b5741c39c1cadd
danmuck <danmuck426@gmail.com>
2022-02-16 11:33:25 -0500

    test: Blockchain:chain_ctl :: validation tweaks

--- modules/chain_ctl/Blockchain.py
+++ modules/chain_ctl/Blockchain.py
@@ -67,6 +67,7 @@ class Blockchain_:
         elif len(self.chain) > len(chain_) and list(chain_.keys()) in list(self.chain.keys()):
             # prepare to diverge
             self.chain_id+=1
+            print("!Err Chain height surpassed master::Diverging...  !!")
             self.validate_chain()
         else:
             # take the chain on file
