88ba6614a35ccef1c077acf1d917ad3aab5a6978
danmuck <danmuck426@gmail.com>
2022-02-20 09:30:53 -0500

    re: nuke_data :: complete nuke

--- nuke_data.sh
+++ nuke_data.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 rm -rf chain_data/ minter_data/;
+
+read -p "This option will also nuke Master_chain and Wallet data.. Clean all? (y/N): " clean_all
+if [ "$clean_all" == "y" ]
+then
+    rm -rf Master_chain.json user_data/
+elif [ "$clean_all" == "Y" ]
+then
+    rm -rf Master_chain.json user_data/
+elif [ "$clean_all" == "" ]
+then
+    :
+else
+    :
+fi
+
 ./ctl_center.py
