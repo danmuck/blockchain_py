90ed70c8525bc8cd96e851936226a71ebc2693c0
danmuck <danmuck426@gmail.com>
2022-02-19 22:29:14 -0500

    test: Transactions:chain_ctl :: multi-wallet tweaks

--- ctl_center.py
+++ ctl_center.py
@@ -84,7 +84,7 @@ def wallet_quick_login(new_=False, w_index:int=0):
 def wallet_login():
     global WALLET
     wallet_quick_login()
-    print('Which wallet would you like to login with?')
+    print('Which wallet would you like to use?')
     i = 0
     for wallet in WALLET.print_wallets(False):
         print(f'{i}. {wallet}')
@@ -312,43 +312,6 @@ def dirt_ranch_welcome():
         WALLET.print_wallets()
     pass
 
-
-
-
 def main():
     dirt_ranch_welcome()
-    # bc = Blockchain_(99)
-    # work = Proof_of_Work(bc)
-    # minter = Minter_(bc, iters_=120000)
-    # minter.generator()
-    # # minter.check_for_uniques()
-    # minter.update_history_json()
-    # minter.history_counts()
-    # print("\n\n-- [end] --\n\nCHAIN: " ,json.dumps(bc.chain, indent=2))
-    # print("HEIGHT: ", len(bc.chain))
-    # print(bc.hash_chain_())
-    # # work.mine_block(txns=["FAKER hehe :)", "8==D"], chain_data={"Im_data": "Liar, he's a my key, IM data... maybe we're both data..."})
-    # # print(bc.hash_chain_())
-    # test = ez_random().get_()
-    # print(test)
-    # bl = Block_(0,"0xsomestringshehe", 13, [], "stresfd", {})
-    # bc.append_block_(bl)
-    # timer.start_timer()
-    # work.mine_block(bc)
-    # work.mine_block(bc)
-    # work.mine_block(bc)
-    # i = 0
-    # while i < 10:
-    #     work.mine_block(bc)
-    #     i+=1
-    # print("TIME: " ,timer.end_timer(), "sec")
-
-    # menu.main_menu_()
-    # ri = randint(0, 9999)
-    # nf = No_fun(ri)
-    # checks_ = nf.get_attrs()
-
-    # work.mine_block(txns=["some", "testing :)"], chain_data=checks_)
-    
-
 main()
